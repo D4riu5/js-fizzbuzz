e412c486fe21325c035cce307d91e6f663a8fa05
D4riu5 <R3iji94@gmail.com>
2022-12-21 14:12:40 +0100

    edited console.log print for FizzBuzz Buzz and Fizz

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ for (let i = 1; i <= 100; i++) {
   
   // if i can be divided by 5 AND divided by 3, print fizz buzz
   if (i % 5 == 0 && i % 3 == 0) {
-    console.log("numero " + i + " FIzzBuzz")
+    console.log("FizzBuzz")
   }
 
   // but if i can be divided 5 Only, print buzz
   else if (i % 5 == 0) {
-    console.log("numero " + i + " Buzz")
+    console.log("Buzz")
   }
 
   // but if i can be divided 3 Only, print buzz
   else if (i % 3 == 0) {
-    console.log("numero " + i + " Fizz")
+    console.log("Fizz")
   }
 
   //otherwise just print i number
